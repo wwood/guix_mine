08162202765e4876f295ec04e7fb3a670ad4fb4c
Ben Woodcroft <donttrustben@gmail.com>
2015-06-21 21:50:53 +1000

    changes, krona sorta works

--- ben/packages/local.scm
+++ ben/packages/local.scm
@@ -21,6 +21,7 @@
   #:use-module (gnu packages linux)
   #:use-module (gnu packages maths)
   #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages pcre)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages popt)
@@ -87,51 +88,6 @@ intervals from multiple files in widely-used genomic file formats such as BAM,
 BED, GFF/GTF, VCF.")
     (license license:gpl2)))
 
-(define-public diamond
-  (package
-    (name "diamond")
-    (version "0.7.9")
-    (source (origin
-              (method url-fetch)
-              (uri
-               (string-append
-                "https://github.com/bbuchfink/diamond/archive/v" version ".tar.gz"))
-              (sha256
-               (base32
-                "0hfkcfv9f76h5brbyw9fyvmc0l9cmbsxrcdqk0fa9xv82zj47p15"))))
-    (build-system gnu-build-system)
-    (arguments
-     '(#:tests? #f ;no "check" target
-		#:phases
-		(alist-cons-after
-		 'unpack 'enter-source-dir
-		 (lambda _ (chdir "src"))
-		 (alist-replace
-		  'install
-		  (lambda* (#:key outputs #:allow-other-keys)
-			   (let ((bin (string-append
-				       (assoc-ref outputs "out") "/bin")))
-			     (mkdir-p bin)
-			     (copy-file "../bin/diamond" (string-append bin "/diamond"))))
-		  (alist-delete 'configure %standard-phases)))))
-    (native-inputs
-     `(("bc" ,bc)))
-    (inputs
-     `(("boost" ,boost)
-       ("zlib" ,zlib)))
-    (home-page "https://github.com/bbuchfink/diamond")
-    (synopsis "Accelerated BLAST compatible local sequence aligner")
-    (description
-     "DIAMOND is a BLAST-compatible local aligner for mapping protein and 
-translated DNA query sequences against a protein reference database (BLASTP 
-and BLASTX alignment mode). The speedup over BLAST is up to 20,000 on short 
-reads at a typical sensitivity of 90-99% relative to BLAST depending on the 
-data and settings.")
-    (license (license:non-copyleft (string-append "https://github.com/bbuchfink"
-						  "/diamond/blob/v"
-						  version
-						  "/src/COPYING")))))
-
 (define-public bamm
   (package
    (name "bamm")
@@ -140,32 +96,197 @@ data and settings.")
 	    (method url-fetch)
 	    (uri (string-append
 		  "file:///tmp/bamm.tar.gz"))
-		  ;"https://github.com/Ecogenomics/BamM/archive/v"
-		  ;version ".tar.gz"))
+		  ;;"https://github.com/Ecogenomics/BamM/archive/v"
+		  ;;version ".tar.gz"))
+	    (file-name (string-append name "-" version ".tar.gz"))
 	    (sha256
 	     (base32
 	      "1vdnc1r8z151h2r1bryl8ylqxggyah6hi84cvyfqhdkqsabjvcx4"))))
    (build-system python-build-system)
-   (arguments `(#:python ,python-2)) ; no Python 3 support
+   (arguments
+    `(#:python ,python-2 ;; no Python 3 support
+      #:phases
+      (modify-phases %standard-phases
+	(replace 'build
+		 (lambda* (#:key inputs #:allow-other-keys)
+		   (let ((htslib (assoc-ref inputs "htslib"))) ;;TODO: use guix
+							       ;;htslib not the
+							       ;;bundled one,
+							       ;;currently bamm
+							       ;;doesn't respect
+							       ;;this
+		     (substitute* "setup.py"
+		       (("if 'sdist' not in sys.argv")
+			"if False"))
+
+		     (chdir "c")
+		     (system* "autoreconf" "--install")
+		     (substitute* "configure"
+		       (("/bin/sh") (which "bash")))
+		     (substitute* "libcfu-0.03/configure"
+		       (("/bin/sh") (which "bash")))
+		     (substitute* "htslib-1.2.1/configure"
+		       (("/bin/sh") (which "bash")))
+		     (system* "bash" "configure")
+		     (system* "make")
+		       
+		     (copy-file "libBamM.a" "../bamm/libBamM.a")
+		     (chdir "..")
+		     #t
+		     )))
+	(delete 'check)
+	)))
    (inputs
     `(("bwa" ,bwa)
-      ("samtools-1" ,samtools-0.1)
+      ("samtools-0.1" ,samtools-0.1)
+      ("htslib" ,htslib)
+      ("zlib" ,zlib)
       ("python-numpy" ,python-numpy)))
    (native-inputs
     `(("autoconf" ,autoconf)
       ("automake" ,automake)
       ("libtool" ,libtool)
-      ("htslib" ,htslib)
       ("python-nose" ,python2-nose)
       ("python-setuptools" ,python2-setuptools)))
    (home-page "http://ecogenomics.github.io/BamM/")
    (synopsis "Metagenomics-focused BAM file manipulation")
    (description
-    "BamM is a c library, wrapped in python, that parses BAM files. The code
-is intended to provide a faster, more stable interface to parsing BAM files than
+    "BamM is a c library, wrapped in python, that parses BAM files. The code is
+intended to provide a faster, more stable interface to parsing BAM files than
 PySam, but doesn't implement all/any of PySam's features. Do you want all the
 links that join two contigs in a BAM? Do you need to get coverage? Would you
 like to just work out the insert size and orientation of some mapped reads? Then
 BamM is for you!")
    (license license:lgpl3+)))
 
+(define-public fxtract
+  (let ((commit "a90a6a84f"))
+    (package
+      (name "fxtract")
+      (version "1.2-dev")
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/ctSkennerton/fxtract.git")
+                      (commit commit)
+                      (recursive? #t)))
+                (sha256
+                 (base32
+                  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa1yck5xv0arrzg83q6v90430"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:tests? #f ;no check target
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (replace 'install
+                  (lambda* (#:key outputs #:allow-other-keys)
+                    (let ((bin (string-append (assoc-ref outputs "out")
+                                              "/bin")))
+                      (mkdir-p bin)
+                      (copy-file "fxtract"
+                                 (string-append bin "/fxtract"))
+                      #t)))
+	 (replace 'check
+		  (lambda* _
+		    (zero? (system* "make"
+				    "test_fxtract"))))
+	 )))
+    (inputs ;;BOOST?
+     `(("pcre" ,pcre)
+       ("bzip2" ,bzip2)
+       ("zlib" ,zlib)))
+    (home-page "https://github.com/ctSkennerton/fxtract")
+    (synopsis "Extract sequences from a fastx file given a subsequence or identifier")
+    (description
+     "Extract sequences from a fastx (fasta or fastq) file given a subsequence.
+Currently uses a variety of search algorithms depending on the task.  Currently
+searches using a simple substring search for basic tasks but can change to using
+POSIX regular expressions, PCRE, hash lookups or multi-pattern searching as
+required.  By default will look in the sequence of each record but can be told
+to look in the header, comment or quality sections of a record.")
+    (license license:gpl3+))))
+
+(define-public hmmer
+  (package
+    (name "hmmer")
+    (version "3.1b2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "http://selab.janelia.org/software/hmmer3/3.1b2/hmmer-"
+		    ;;"file:///tmp/ksf73acji56crmvhdzlayyvx9fmhbxx7-hmmer-"
+		    version ".tar.gz"))
+	      (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0djmgc0pfli0jilfx8hql1axhwhqxqb8rxg2r5rg07aw73sfs5nx"))))
+    (build-system gnu-build-system)
+    (native-inputs `(("perl", perl)))
+    (home-page "http://hmmer.janelia.org")
+    (synopsis "Biosequence analysis using profile hidden Markov models")
+    (description
+     "HMMER is used for searching sequence databases for homologs of protein
+sequences, and for making protein sequence alignments. It implements methods
+using probabilistic models called profile hidden Markov models (profile HMMs).")
+    (license license:gpl3+)))
+
+
+(define-public kronatools
+  (package
+    (name "kronatools")
+    (version "2.5")
+    (source (origin
+	      (method url-fetch)
+	      (uri (string-append
+		    "http://sourceforge.net/projects/krona/files/KronaTools"
+		    "%20%28Mac%2C%20Linux%29/KronaTools-"
+		    version ".tar/download"))
+	      (file-name (string-append name "-" version ".tar"))
+	      (sha256
+	       (base32
+		"12ps0y7p19d3cdhc7pp3xlak5k9qq5w246861bngfg8mkfa083qa"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:tests? #f
+       #:phases
+       (modify-phases %standard-phases
+	 (delete 'configure)
+
+	 ;;TODO: remove the underscore files in e.g. the scripts directory
+	 (delete 'build)
+	 ;;	 (replace 'build 
+	 ;; (lambda* _
+	 ;;   (define (leaf name stat result)
+	 ;;     (pretty-print name)(+0))
+	 ;;   (file-system-fold #t leaf _ _ _ _ 0 ".")
+	 ;;   #t))
+	 (replace 'install
+		  (lambda* (#:key outputs #:allow-other-keys)
+		    (let ((out (string-append (assoc-ref outputs "out"))))
+		      (mkdir-p out)
+		      (copy-recursively "lib" (string-append out "/lib"))
+		      (copy-recursively "scripts" (string-append out "/scripts"))
+		      (copy-recursively "data" (string-append out "/data"))
+		      (copy-recursively "img" (string-append out "/img"))
+		      (copy-recursively "src" (string-append out "/src"))
+		      (copy-recursively "taxonomy" (string-append out "/taxonomy"))
+		      (copy-file "install.pl" (string-append out "/install.pl"))
+		      (chdir out)
+		      (system* "perl"
+			       "install.pl"
+			       "--prefix"
+			       out)
+		      (delete-file "install.pl")))))))
+    (inputs
+     `(("perl", perl)))
+    (home-page "http://sourceforge.net/projects/krona")
+    (synopsis "Hierarchical data exploration with zoomable HTML5 pie charts")
+    (description
+     "Krona is a flexible tool for exploring the relative proportions of
+hierarchical data, such as metagenomic classifications, using a radial,
+space-filling display.  It is implemented using HTML5 and JavaScript, allowing
+charts to be explored locally or served over the Internet, requiring only a
+current version of any major web browser.")
+    (license (license:non-copyleft "file://src/LICENSE"
+                                   "See src/LICENSE in the distribution."))))

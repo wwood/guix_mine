0cb84ae10b237d072cfdc4c4bcbd14e94c093b52
Ben Woodcroft <donttrustben@gmail.com>
2017-11-23 21:18:38 +1000

    Add r-ggsignif.

--- ben/packages/local.scm
+++ ben/packages/local.scm
@@ -6202,3 +6202,23 @@ extract population genomes from multi-sample metagenomic datasets.")
 
 (define-public python2-squarify
   (package-with-python2 python-squarify))
+
+(define-public r-ggsignif
+  (package
+   (name "r-ggsignif")
+   (version "0.4.0")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (cran-uri "ggsignif" version))
+     (sha256
+      (base32
+       "1rn58d7pb3axk6chiihryykrzw76adaa2yiafq4d0j6qbhax78f7"))))
+   (build-system r-build-system)
+   (propagated-inputs `(("r-ggplot2" ,r-ggplot2)))
+   (home-page
+    "https://github.com/const-ae/ggsignif")
+   (synopsis "Significance Brackets for 'ggplot2'")
+   (description
+    "Enrich your 'ggplots' with group-wise comparisons.  This package provides an easy way to indicate if two groups are significantly different.  Commonly this is shown by a bracket on top connecting the groups of interest which itself is annotated with the level of significance (NS, *, **, ***).  The package provides a single layer (geom_signif()) that takes the groups for comparison and the test (t.test(), wilcox.text() etc.) as arguments and adds the annotation to the plot.")
+   (license license:gpl3)))
